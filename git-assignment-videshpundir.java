458abf8d0f3daacaca432f67f41dcf7a1608a314
VideshTech34 <videsh5655@gmail.com>
2023-01-29 03:58:47 +0530

    first commit

---
+++ git-assignment-videshpundir.java
@@ -0,0 +1,74 @@
+//Git and GitHub Assignment
+
+1. What is Git?
+Ans->Git is a popular version control system(VCS), It was created by Linus Torvalds
+in 2005 and has been maintained by Junio Hamano
+git is mainly used for :
+*Tracking code changes
+*Tracking who made changes like history of the files.
+*Coding Collaboratons
+
+2.What do you understand by the term  "Verson Control System"?
+Ans->It is a system that records changes to a file or set of files over time, so that
+we can recall specific versions later, i.e., for every source code changes in a
+file a new version will be created.
+
+The Version Control System is very helpful and beneficial in software development; developing software without using version control is unsafe.
+ It provides backups for uncertainty. Version control systems offer a speedy interface to developers.
+ It also allows software teams to preserve efficiency and agility according to the team scales to include more developers.
+There are three types of version control system
+* localized version control system.
+* centralized version control systems.
+* distributed version control systems.
+
+3.What is GitHub?
+
+Ans->GitHub is a code hosting platform for collaboration and version control.
+     GitHub lets you (and others) work together on projects.
+
+4.Menton some popular git hosting services.
+Ans-> These are the some popular git hosting services.
+ *Bitbucket
+ *GitHub
+ *GitLab
+ *Perforce
+ *Beanstalk
+ *Amazon AWS CodeCommit
+ *Codebase
+ *Microsoft Azure DevOps
+ *SourceForge
+ *Gerrit
+5.What are the different type of version control systems?
+
+Ans->There are two types of version control systems which we use mainly.
+1 Centralized Version Control System (CVCS)
+2. Distributed Version Control System (DVCS)
+
+cvcs->Developers can collaborate and do the changes
+example->: CVS, SubVersion,Perforce
+dvcs->
+ DVCS are Git, Mercurial,Darcs, Bazaar,...etc
+ Developers don't check out the latest snapshot of the files rather they fully mirror
+ the repository including its full history.
+ If the main server dies, then the local system will maintain a copy of the main
+ repository which has full backup of data.
+ If the remote repository is down, then the developer can make changes in the
+ local repository and when the main repository is up the code can be pushed to
+ remote repository from local repository
+
+6. What benefit come using git?
+Ans->
+a. Tracking code changes
+b. Tracking who made changes like history of the files
+c. Coding Collaborations
+
+7. What is Git repository?
+Ans->A Git repository tracks and saves the history of all changes made to the files in a Git project.
+ It saves this data in a directory called .git,
+ also known as the repository folder.Git uses a version control system to track all changes made to the project and save them in the repository.
+ Users can then delete or copy existing repositories or create new ones for ongoing projects.
+
+8. How can u initialise git repository?
+Ans->To create (initialize) a local Git repository for a project, go to the root in our project and run this command:
+
+git init
